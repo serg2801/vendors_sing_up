c4a18212a208de1265b0e034d03c0112cf1654b7
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-05-03 12:36:31 +0300

    edit mailers default from and add reply_to and testing attachments

--- app/mailers/trade_mailer.rb
+++ app/mailers/trade_mailer.rb
@@ -1,15 +1,16 @@
 class TradeMailer < ApplicationMailer
-  default from: "hello@tandemarbor.com"
+  default from: "donotreply@tandemarbor.com"
 
   def signup_confirmation(trade)
     @trade = trade
-    attachments["#{@trade.image}"] = File.read("#{Rails.root}/public/#{@trade.image}")
+    attachments["#{@trade.image}"] = File.read("#{Rails.root}/public/#{@trade.image}") if @trade.image != nil
     mail to: "trade@tandemarbor.com", subject:  "Vendor Form Submission " + "#{@trade.business_name}"
   end
 
   def send_confirmation(trade)
     @trade = trade
     mail to: trade.email,
+         reply_to: "vendors@tandemarbor.com",
          subject:  "Vendor Form Submission"
   end
 
--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -1,15 +1,16 @@
 class VendorMailer < ApplicationMailer
-  default from: "hello@tandemarbor.com"
+  default from: "donotreply@tandemarbor.com"
 
   def signup_cinfirmation(vendor)
     @vendor = vendor
-    attachments["#{@vendor.image}"] = File.read("#{Rails.root}/public/#{@vendor.image}")
+    attachments["#{@vendor.image}"] = File.read("#{Rails.root}/public/#{@vendor.image}") if @vendor.image != nil
     mail to: "trade@tandemarbor.com", subject:  "Trade Form Submission " + "#{@vendor.business_name}"
   end
 
   def send_confirmation(vendor)
     @vendor = vendor
     mail to: vendor.email,
+         reply_to: "trade@tandemarbor.com",
          subject:  "Trade Form Submission"
   end
 
