631f0ef810ff0e99d4f3a046c7731e384404a4e8
Sergei Koretskiy <skoretskiy@webiprog.com>
2016-05-27 15:03:20 +0000

    edit migrate type for add column for board

--- db/migrate/20160527122730_add_columns_to_boards.rb
+++ db/migrate/20160527122730_add_columns_to_boards.rb
@@ -1,11 +1,11 @@
 class AddColumnsToBoards < ActiveRecord::Migration
   def change
-    add_column :boards, :inventory_integration_method_other, :string
+    add_column :boards, :inventory_integration_method_other, :text
 
-    add_column :boards, :contact_other_title, :string
-    add_column :boards, :contact_other_name, :string
-    add_column :boards, :contact_other_phone, :string
-    add_column :boards, :contact_other_email, :string
-    add_column :boards, :contact_other_fax, :string
+    add_column :boards, :contact_other_title, :text
+    add_column :boards, :contact_other_name, :text
+    add_column :boards, :contact_other_phone, :text
+    add_column :boards, :contact_other_email, :text
+    add_column :boards, :contact_other_fax, :text
   end
 end
