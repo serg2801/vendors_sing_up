0081781d846e662abe62c66e52329bdda73eb459
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-06-01 17:31:32 +0300

    move buttons

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -53,18 +53,61 @@ class TradesController < ApplicationController
   def update
     @user = User.find(current_user.id)
     @trade = @user.trade
-    if @trade.update_attributes(trade_params)
-      TradeMailer.update_trade(@trade).deliver
-      redirect_to trade_success_update_path
+    @categories = params[:trade][:category_ids]
+    @channels = params[:trade][:channel_ids]
+    @options = params[:trade][:option_ids]
+
+    if @categories.nil?
+      flash[:warning] = "Please, indicate the options that describe your business!"
+      render 'new'
+    elsif @channels.nil?
+      flash[:warning] = "Please, indicate what categories would you like to see on Tandem Arbor!"
+      render 'new'
+    elsif @options.nil?
+      flash[:warning] = "Please, indicate What channel(s) do you currently sell through!"
+      render 'new'
     else
-      render :edit
+      @trade.categories.each do |category|
+        @trade_category = TradeCategory.find_by(category_id: category.id, trade_id: @trade.id)
+        @trade_category.destroy
+      end
+      @categories.each do |category|
+        @category = Category.find(category)
+        @trade.categories << @category
+      end
+
+      @trade.channels.each do |channel|
+        @trade_channel = TradeChannel.find_by(channel_id: channel.id, trade_id: @trade.id)
+        @trade_channel.destroy
+      end
+      @channels.each do |channel|
+        @channel = Channel.find(channel)
+        @trade.channels << @channel
+      end
+
+      @trade.options.each do |option|
+        @trade_option = TradeOption.find_by(option_id: option.id, trade_id: @trade.id)
+        @trade_option.destroy
+      end
+      @options.each do |option|
+        @option = Option.find(option)
+        @trade.options << @option
+      end
+
+      if @trade.update_attributes(trade_params)
+        TradeMailer.update_trade(@trade).deliver
+        redirect_to trade_success_update_path
+      else
+        render :edit
+      end
     end
   end
 
   private
   def trade_params
     params.require(:trade).permit(:business_name, :greeting, :first_name, :last_name, :email, :phone_number, :address,
-                                   :suite, :city, :state, :zipcode, :country, :web_site_url_my, :web_site, :information, :image)
+                                  :suite, :city, :state, :zipcode, :country, :web_site_url_my, :web_site, :information,
+                                  :image, :option_ids, :category_ids, :channel_ids)
   end
 
 
