84d1889295f77e06ad46a8ede354a3cd6c34cf16
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-06-03 13:07:41 +0300

    fix select and test nested-fields for add form

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -1,4 +1,4 @@
-$(document).on("page:change",function () {
+$(document).on("page:change", function () {
 
     $("#add_brand_name").trigger("click");
     $(".nested-fields .brand_name").find(".remove_brand_js").hide();
@@ -41,93 +41,126 @@ $(document).on("page:change",function () {
         $("#contact_other_js").toggle();
     });
 
-    $("#remove_contact_order_js").click(function() {
+    $("#remove_contact_order_js").click(function () {
         $("#contact_order_js").toggle();
     });
 
-    $("#remove_contact_inventory_js").click(function() {
+    $("#remove_contact_inventory_js").click(function () {
         $("#contact_inventory_js").toggle();
     });
 
-    $("#remove_contact_return_js").click(function() {
+    $("#remove_contact_return_js").click(function () {
         $("#contact_return_js").toggle();
     });
 
-    $("#remove_contact_information_js").click(function() {
+    $("#remove_contact_information_js").click(function () {
         $("#contact_information_js").toggle();
     });
 
-    $("#remove_contact_service_js").click(function() {
+    $("#remove_contact_service_js").click(function () {
         $("#contact_service_js").toggle();
     });
 
-    $("#remove_contact_other_js").click(function() {
+    $("#remove_contact_other_js").click(function () {
         $("#contact_other_js").toggle();
     });
 
     $(".form-group .radio_button_js").click(function () {
-        if (document.getElementById("radio_button_other_js").checked)
-        {
+        if (document.getElementById("radio_button_other_js").checked) {
             $("#form_other_js").show();
-        }else
-        {
+        } else {
             $("#form_other_js").hide();
             $("#form_other_js").val("");
         }
     });
 
     $(".form-group .radio_inventory_js").click(function () {
-        if (document.getElementById("radio_inventory_js").checked)
-        {
+        if (document.getElementById("radio_inventory_js").checked) {
             $("#form_other_inventory_js").show();
-        }else
-        {
+        } else {
             $("#form_other_inventory_js").hide();
             $("#form_other_inventory_js").val("");
         }
     });
 
     $(".form-group .radio_button_method_js").click(function () {
-        if (document.getElementById("radio_method_other_js").checked)
-        {
+        if (document.getElementById("radio_method_other_js").checked) {
             $("#form_method_other_js").show();
-        }else
-        {
+        } else {
             $("#form_method_other_js").hide();
             $("#form_method_other_js").val("");
         }
     });
 
     $(".form-group .radio_button_frequency_js").click(function () {
-        if (document.getElementById("radio_frequency_other_js").checked)
-        {
+        if (document.getElementById("radio_frequency_other_js").checked) {
             $("#form_frequency_other_js").show();
-        }else
-        {
+        } else {
             $("#form_frequency_other_js").hide();
             $("#form_frequency_other_js").val("");
         }
     });
     $(".form-group .radio_button_costs_fees_js").click(function () {
-        if (document.getElementById("radio_costs_fees_yes_js").checked)
-        {
+        if (document.getElementById("radio_costs_fees_yes_js").checked) {
             $(".form_costs_fees_other_js").show();
-        }else
-        {
+        } else {
             $(".form_costs_fees_other_js").hide();
             //$(".form_costs_fees_other_js").val("");
             $(".form_costs_fees_other_js .costs_fees").val("");
         }
     });
     $(".form-group .radio_button_purchase_orders_js").click(function () {
-        if (document.getElementById("radio_purchase_orders_yes_js").checked)
-        {
+        if (document.getElementById("radio_purchase_orders_yes_js").checked) {
             $(".purchase_orders_block_js").show();
-        }else
-        {
+        } else {
             $(".purchase_orders_block_js").hide();
             $(".purchase_orders_block_js .purchase_order").val("");
         }
     });
 
+
+    $('#brand_name_form_list')
+        .on('cocoon:before-insert', function () {
+            $("#brand_name_form_list .field input:text").each(function () {
+                var input = $(this);
+                if (input.val() == "") {
+                    input.parents('.nested-fields').find(".remove_brand_js").trigger("click");
+                }
+            });
+        });
+        //.on('cocoon:after-insert', function () {
+        //    if ($("#brand_name_form_list .field input:text").length === 1) {
+        //        $("#brand_name_form_list .field input:text").parents('.nested-fields').find(".brand_name .remove_brand_js").hide();
+        //    }
+        //});
+
+    $('#another_carrier_form_list')
+        .on('cocoon:before-insert', function () {
+            $("#another_carrier_form_list .field input:text").each(function () {
+                var input = $(this);
+                if (input.val() == "") {
+                    input.parents('.nested-fields').find(".remove_another_carrier_js").trigger("click");
+                }
+            });
+        });
+
+    $('#another_product_type_form_list')
+        .on('cocoon:before-insert', function () {
+            $("#another_product_type_form_list").find(":selected").each(function () {
+                var input = $(this);
+                if (input.val() == "") {
+                    input.parents('.nested-fields').find(".remove_another_product_type_js").trigger("click");
+                }
+            });
+        });
+
+    $('#another_location_form_list')
+        .on('cocoon:before-insert', function () {
+            $("#another_location_form_list .field input:text").each(function () {
+                var input = $(this);
+                if (input.val() == "") {
+                    input.parents('.nested-fields').find(".remove_ship_information_js").trigger("click");
+                }
+            });
+        });
 });
